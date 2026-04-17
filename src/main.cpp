53f365b1dceb8279042958aca894d75feb797887
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-04-17 12:38:52 -0700

    Add first raylib stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <cstdint>
+#include <raylib.h>
+
+struct WindowDetails {
+    const int32_t width, height;
+    const char* title;
+};
 
 int main() {
-    std::cout << "hello, world\n";
+    WindowDetails window{
+        .width = 640, .height = 480,
+        .title = "Pong"
+    };
+
+    InitWindow(window.width, window.height, window.title);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
